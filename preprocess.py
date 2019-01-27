cc60291642e9a1ed061cb4d7340c9ff63a945bfa
demacdolincoln <lincoln000@openmaibox.org>
2019-01-27 07:58:41 -0300

    func cleantext added

---
+++ preprocess.py
@@ -0,0 +1,27 @@
+import glob
+
+import gensim
+from nltk.corpus import stopwords
+
+
+def cleantext(path, lang="english"):
+    corpus, target = [], []
+
+    stopw = stopwords.words(lang)
+    for file in glob(path):
+        texts = open(file).read()
+        i = 0
+        for line in texts.splitlines():
+
+            try:
+                target.append(int(line.split("\t")[-1]))
+
+                sc = gensim.utils.simple_preprocess(line)  # sentence candidate
+                sc = [i for i in sc if i not in stopw]
+                corpus.append(sc)
+            except:
+                print("encode error in: ", file, i)
+            i += 1
+    assert len(corpus) == len(target),\
+     f"corpus: {len(corpus)} | target: {len(target)}"
+    return corpus, target
