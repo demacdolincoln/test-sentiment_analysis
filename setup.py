67e10c0e95efe905ffbf8cfc7f6b1c7dca614328
demacdolincoln <lincoln000@openmaibox.org>
2019-01-29 19:50:16 -0300

    saved vec :ok:

--- setup.py
+++ setup.py
@@ -1,43 +1,56 @@
 CONFIG = {
     "2d": {
-        "m": max_sentence,
-        "n": max_sentence,
-        "path": f"word2vec/skipgram_{max_sentence}.vec",
+        "m": 44,
+        "n": 44,
+        "vec_path": f"word2vec/skipgram_44.vec",
         "epochs": 100,
         "lr": 1e-3,
         "save_path": "saved_models/conv2d.pth"
     },
     "1d": {
-        "m": max_sentence,
+        "m": 44,
         "n": 10,
-        "path": f"word2vec/skipgram_10.vec",
+        "vec_path": f"word2vec/skipgram_10.vec",
         "epochs": 100,
         "lr": 1e-3,
         "save_path": "saved_models/conv1d.pth"
-    }
+    },
     "rnn": {
-        "m": max_sentence,
+        "m": 44,
         "n": 10,
-        "path": f"word2vec/skipgram_10.vec",
+        "vec_path": f"word2vec/skipgram_10.vec",
         "epochs": 100,
         "lr": 1e-4,
         "save_path": "saved_models/rnn_{_type}.pth"
     },
     "conv_1d_rnn": {
-        "m": max_sentence,
+        "m": 44,
         "n": 10,
-        "path": f"word2vec/skipgram_10.vec",
+        "vec_path": f"word2vec/skipgram_10.vec",
         "epochs": 100,
         "lr": 1e-5,
         "save_path": "saved_models/conv1d_rnn.pth"
     },
     "conv_2d_rnn": {
-        "m": max_sentence,
-        "n": max_sentence,
-        "path": f"word2vec/skipgram_10.vec",
+        "m": 44,
+        "n": 44,
+        "vec_path": f"word2vec/skipgram_44.vec",
         "epochs": 100,
         "lr": 1e-5,
         "save_path": "saved_models/conv1d_rnn.pth"
     }
 }
 
+
+if __name__ == "__main__":
+
+    from preprocess import cleantext, save_word2vec
+
+    ex_1d = CONFIG["1d"]
+    ex_2d = CONFIG["2d"]
+
+    corpus, target, stopw = cleantext("dataset/*labelled.txt")
+    for i in [ex_1d, ex_2d]:
+        save_word2vec(corpus, i["n"], 1,
+                      2000, i["vec_path"])
+        print(i["vec_path"], ": ok")
